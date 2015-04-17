289919cd0304a6171eded238c43e2a8458f93f8c
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-04-17 02:48:52 -0300

    remove favicon problem

--- index.js
+++ index.js
@@ -4,8 +4,16 @@
       app     = express();
 
   // Heroku port settings
-  app.set('port', (process.env.PORT || 8080));
+  app.set('port', (process.env.port || 8080));
   app.use(express.static(__dirname));
+  app.use(function(req, res, next){
+    if (req.url === '/favicon.ico') {
+      res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+      res.end('');
+    } else {
+      next();
+    }
+  });
 
   app.use('/', require('./routes'));
 
