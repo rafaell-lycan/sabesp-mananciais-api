a08bf2386064359661d7676fa5accbdc879a5010
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-05-28 00:51:04 +0200

    Update application file

--- src/application.ts
+++ src/application.ts
@@ -1,17 +1,21 @@
+import { env, cwd } from 'process';
+import { join } from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
+import favicon from 'serve-favicon';
+import morgan from 'morgan';
 import express, { Application } from 'express';
-import helmet = require('helmet');
-import * as http from 'http';
+import { useExpressServer } from 'routing-controllers';
 import { MongoClient, MongoClientOptions } from 'mongodb';
-import { env } from 'process';
 
-import Analytics from './common/middleware/analytics';
-import { errorHandler, notFoundHandler } from './common/middleware/handlers';
+import logger from './common/utils/logger';
 import swagger from './common/middleware/swagger';
-import Logger from './common/utils/logger';
-import routes from './routes';
-
-const logger = Logger('app');
+import Analytics from './common/middleware/analytics';
+import NotFoundHandler from './common/middleware/NotFoundHandler';
+import ErrorHandler from './common/middleware/ErrorHandler';
+import IndexController from './api';
+import ApiV2Controller from './api/v2';
+import ApiV1Controller from './api/v1';
 
 export default class Server {
   private static app: Application;
@@ -28,12 +32,12 @@ export default class Server {
   private setup(app: Application): void {
     this.database(app);
     this.middleware(app);
-    this.routes(app);
+    this.controllers(app);
   }
 
   private async database(app: Application): Promise<void> {
     if (env.MONGO_URI) {
-      logger('Adding MongoDB configuration...');
+      logger.debug('Adding MongoDB configuration...');
       const dbOptions: MongoClientOptions = { useNewUrlParser: true };
       const db: MongoClient = await MongoClient.connect(env.MONGO_URI, dbOptions);
       app.set('db', db);
@@ -42,39 +46,52 @@ export default class Server {
 
   // Configure Express middleware.
   private middleware(app: Application): void {
-    logger('Applying middlewares...');
-
-    // enable helmet
-    app.use(helmet());
-
-    // enable cors
-    app.use(cors({ maxAge: 600 }));
+    logger.debug('Applying middlewares...');
 
     // enable swagger
     swagger(app);
 
-    // enable Analytics
-    if (env.ANALYTICS) {
-      app.use(new Analytics(env.ANALYTICS).track);
-    }
+    // enable helmet
+    app.use(helmet());
+
+    // enable server favicon
+    app.use(favicon(join(cwd(), 'public', 'favicon.ico')));
+
+    // enable cors
+    app.use(cors());
+
+    // enable morgan logger
+    app.use(morgan('dev'));
   }
 
-  // Configure routes
-  private routes(app: Application): void {
-    routes(app);
+  // Configure routes based on controllers
+  private controllers(app: Application): void {
+    logger.debug('Configuring routes...');
+    // const controllers = [`${__dirname}/controllers/*`];
 
-    // Add application error handlers
-    // app.use(notFoundHandler);
-    // app.use(errorHandler);
+    useExpressServer(app, {
+      controllers: [IndexController, ApiV2Controller, ApiV1Controller],
+      middlewares: [
+        // custom middlewares
+        Analytics,
+        NotFoundHandler,
+        ErrorHandler,
+      ],
+      defaultErrorHandler: false,
+    });
   }
 
   public start(): void {
     const port: string | number = env.PORT || 3000;
 
-    http.createServer(Server.app).listen(port, () => {
-      logger(`
-        Your server is running on http://localhost:${port}
-        Swagger-ui is available on http://localhost:${port}/docs
+    Server.app.listen(port, () => {
+      logger.info(`
+      ------------
+      Server Started!
+      Express:      http://localhost:${port}
+      Swagger Docs: http://localhost:${port}/swagger/docs
+      Swagger Spec: http://localhost:${port}/swagger/api-docs
+      ------------
       `);
     });
   }
