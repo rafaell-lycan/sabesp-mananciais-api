c8c77a457a6fa9617d67466fa04c854b6a305add
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-05-30 15:13:20 +0200

    Add tests for the Mapper/Transformers

---
+++ test/unit/Mapper.spec.ts
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+import Mapper from '../../src/services/Mapper';
+import { SabespDam } from './../../src/services/Sabesp';
+import { SabespResponse } from '../../src/services/Sabesp';
+
+describe('Services: Mapper', () => {
+  const systems: Array<Partial<SabespDam>> = [
+    {
+      Nome: 'Cantareira',
+      VolumePorcentagemAR: '100',
+      VolumeVariacaoStr: '1',
+      PrecDia: '1',
+      PrecMensal: '30',
+      PrecHist: '1',
+    },
+  ];
+
+  const response = {
+    ReturnObj: {
+      sistemas: systems,
+    },
+  } as SabespResponse;
+
+  it('should return the V1 format', () => {
+    const result = Mapper.v1(response);
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.eql({
+      name: 'Cantareira',
+      data: [
+        {
+          key: 'volume armazenado',
+          value: '100 %',
+        },
+        {
+          key: 'pluviometria do dia',
+          value: '1 mm',
+        },
+        {
+          key: 'pluviometria acumulada no mês',
+          value: '30 mm',
+        },
+        {
+          key: 'média histórica do mês',
+          value: '1 mm',
+        },
+      ],
+    });
+  });
+
+  it('should return the V2 format', () => {
+    const result = Mapper.v2(response);
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.eql({
+      name: 'Cantareira',
+      data: {
+        volume_armazenado: '100 %',
+        pluviometria_do_dia: '1 mm',
+        pluviometria_acumulada_no_mes: '30 mm',
+        media_historica_do_mes: '1 mm',
+      },
+    });
+  });
+});
